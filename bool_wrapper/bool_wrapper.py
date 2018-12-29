49c621fe44133cbcb64c4f261bdbbb04835c9864
Konrad Kocik <konrad.kocik@gmail.com>
2018-12-29 21:38:20 +0100

    Fix mutation tests

--- bool_wrapper/bool_wrapper.py
+++ bool_wrapper/bool_wrapper.py
@@ -8,8 +8,7 @@ class BoolWrapper:
         :param bool_to_wrap: bool value (or value which could be converted to bool) used for initialization
         :type bool_to_wrap: bool
         """
-        self._wrapped_bool = None
-        self.wrap(bool_to_wrap)
+        self._wrapped_bool = bool(bool_to_wrap)
 
     def __repr__(self):
         return repr(self._wrapped_bool)
